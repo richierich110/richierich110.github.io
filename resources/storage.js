5408c160d8705fd4e5ed46a6a3df38dd69adc2b9
richierich110 <35575264+richierich110@users.noreply.github.com>
2025-06-21 13:20:42 -0400

    added changeStylesheet

--- resources/storage.js
+++ resources/storage.js
@@ -22,14 +22,24 @@ function removePopup(){
   popup.style.display = "none";
 }
 
+function changeStylesheet(newStylesheet){
+  var oldlink = document.getElementsByTagName("link").item(0);
+
+  var newlink = document.createElement("link");
+  newlink.setAttribute("rel", "stylesheet");
+  newlink.setAttribute("href", newStylesheet);
+  
+   document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
+
 function startupConfiguration(){
   if(checkStorage()){
     if(localStorage.colorMode){
       let mode = localStorage.getItem("colorMode");
       if(mode === "dark"){
-
+        changeStylesheet("./dark.css");
       } else{
-
+        changeStylesheet("./light.css");
       }
     } else{
       localStorage.setItem("colorMode", "dark")
