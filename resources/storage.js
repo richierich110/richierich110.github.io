1ce330cb03fa03112772927571204c46ace10fb0
richierich110 <35575264+richierich110@users.noreply.github.com>
2025-06-21 13:03:33 -0400

    added popup

--- resources/storage.js
+++ resources/storage.js
@@ -1,24 +1,6 @@
-function setStorage(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
 
-function getStorage(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
+const body = document.getElementById("body");
+const popButton = document.getElementById("popupButton");
 
 function checkStorage() {
   if (typeof(Storage) !== "undefined") {
@@ -28,20 +10,34 @@ function checkStorage() {
   }
 }
 
+function addPopup(){
+  const popup = document.getElementById("storage");
+  popButton.addEventListener("click", removePopup);
+  popup.style.display = "flex";
+}
+
+function removePopup(){
+  const popup = document.getElementById("storage");
+  popButton.removeEventListener("click", removePopup);
+  popup.style.display = "none";
+}
+
 function startupConfiguration(){
-  const popup = document.getElementById("popup");
   if(checkStorage()){
     if(localStorage.colorMode){
       let mode = localStorage.getItem("colorMode");
       if(mode === "dark"){
 
       } else{
-        
+
       }
     } else{
       localStorage.setItem("colorMode", "dark")
     }
   } else{
-
+    addPopup();
   }
 }
+
+body.addEventListener("load", startupConfiguration);
+popButton.addEventListener("click", removePopup);
